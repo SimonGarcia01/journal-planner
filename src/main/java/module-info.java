932652f1521ef8e71c31fe06d45f1216701f5c07
Simón García <157841549+SimonGarcia01@users.noreply.github.com>
2025-01-01 21:57:29 -0500

    Deleted previous intelliJ structure.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.annsstationery.journalplanner {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.annsstationery.journalplanner to javafx.fxml;
-    exports com.annsstationery.journalplanner;
-}
